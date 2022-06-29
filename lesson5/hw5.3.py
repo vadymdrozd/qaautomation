5a46483ae5456008dbbc39f81fc154fa92002cf6
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-06-29 14:27:46 +0300

    Added hw5.3 solution

---
+++ lesson5/hw5.3.py
@@ -0,0 +1,21 @@
+# Напишіть функцію, що приймає два аргументи. Функція повинна:
+# a. якщо аргументи відносяться до числових типів - повернути різницю цих аргументів,
+# b. якщо обидва аргументи це строки - обʼєднати в одну строку та повернути
+# c. якщо перший - строка, а другий - ні - повернути dict де ключ це перший аргумент, а значення - другий
+# d. у будь-якому іншому випадку повернути кортеж з цих аргументів
+
+def multi_func(arg1, arg2):
+    if isinstance(arg1, (int, float)) and isinstance(arg2, (int, float)):
+        return arg1 - arg2
+    elif isinstance(arg1, str) and isinstance(arg2, str):
+        return arg1 + arg2
+    elif isinstance(arg1, str) and type(arg2) is not str:
+        return {arg1: arg2}
+    else:
+        return arg1, arg2
+
+
+a = multi_func(None, True)
+
+print(type(a))
+print(a)
