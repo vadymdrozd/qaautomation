bdf27103d786452d4fcc40c054a6535e019f8e73
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-07-02 12:03:56 +0300

    Moved executive functionality to the main.py file

---
+++ lesson6/main.py
@@ -0,0 +1,9 @@
+from hw6_1 import tickets_check
+
+while True:
+    try:
+        customer_age = int(input('Будь ласка, введіть ваш вік цілим числом: '))
+        print(tickets_check(customer_age))
+        break
+    except ValueError as e:
+        print('Вибачте, але те, що ви ввели, не є цілим числом. Будь ласка, спробуйте ще раз')
