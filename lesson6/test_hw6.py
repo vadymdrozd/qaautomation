44ed1c4d23b89ff6807acd7af495c02d3aee8dda
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-07-02 12:06:20 +0300

    Rewrited tests according to interesting_age func deleting

--- lesson6/test_hw6.py
+++ lesson6/test_hw6.py
@@ -1,5 +1,4 @@
 from hw6_1 import tickets_check
-from hw6_1 import interesting_age
 
 # def years_typer() testing
 
@@ -18,13 +17,9 @@ def test_from_two_till_four_year():
         assert tickets_check(digit) == f'Тобі ж {digit} роки! Де твої батьки?'
 
 
-def test_yaers_with_two_three_four():
+def test_years_with_two_three_four():
     for digit in [23, 32, 43, 54, 62]:
         assert tickets_check(digit) == f'Незважаючи на те, що вам {digit} роки, білетів все одно нема!'
-
-
-def test_retired():
-    assert tickets_check(66) == f'Вам 66 років? Покажіть пенсійне посвідчення!'
 
 
 def test_retired_with_one():
@@ -32,23 +27,11 @@ def test_retired_with_one():
         assert tickets_check(digit) == f'Вам {digit} рік? Покажіть пенсійне посвідчення!'
 
 
-# def interesting_age() function testing
-
-
-def test_repdigit_with_one():
-    for digit in (11, 111, 1111):
-        assert interesting_age(digit) == f'О, вам {digit} років! Який цікавий вік!'
+def test_repdigit_with_one_or_from_five():
+    for digit in (11, 111, 1111, 55, 66, 77, 88, 99):
+        assert tickets_check(digit) == f'О, вам {digit} років! Який цікавий вік!'
 
 
 def test_repdigit_with_two_three_four():
     for digit in (22, 33, 44, 222):
-        assert interesting_age(digit) == f'О, вам {digit} роки! Який цікавий вік!'
-
-
-def test_repdigit_with_from_five():
-    for digit in (55, 66, 77, 88, 99):
-        assert interesting_age(digit) == f'О, вам {digit} років! Який цікавий вік!'
-
-
-
-
+        assert tickets_check(digit) == f'О, вам {digit} роки! Який цікавий вік!'
