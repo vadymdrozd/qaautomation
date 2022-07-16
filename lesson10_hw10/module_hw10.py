c1710f757fa6d4b32353249c93accf9d5e98183f
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-07-16 16:43:56 +0300

    Fixed hw10 functions

--- lesson10_hw10/module_hw10.py
+++ lesson10_hw10/module_hw10.py
@@ -2,18 +2,12 @@
 # Повернути колекцію, кожен член якої є перетвореним членом вхідної колекції.
 
 
-def multiplication_on_two(collection):
-    arg_list = []
-    for value in collection:
-        arg_list.append(value * 2)
-    return arg_list
+def multiplication_on_two(argument):
+    return [arg * 2 for arg in argument]
 
 
-def dividing_on_two(collection):
-    arg_list = []
-    for value in collection:
-        arg_list.append(value / 2)
-    return arg_list
+def dividing_on_two(argument):
+    return [arg / 2 for arg in argument]
 
 
 def pseudo_map(func, collection):
