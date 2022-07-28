f5b0e384ca1cf531f703910360e2472396608a89
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-07-28 22:11:27 +0300

    Modified hw13

--- hw13/decorated_functions.py
+++ hw13/decorated_functions.py
@@ -1,23 +1,46 @@
-from vacation_decorators import vacation_type, sick_leave_type, day_off_type
-
-
-vacation_types = {
-    1: vacation_type,
-    2: sick_leave_type,
-    3: day_off_type,
-}
+from vacation_decorators import title
 
 name_input = str(input('Please enter your name: '))
 surname_input = str(input('Please enter your surname: '))
-vacation_kind_input = int(input('Please choose kind of vacation you want (enter a number)? '
-                          '1 - Vacation, 2 - Sick leave, 3 - Day off: '))
-from_date = input('Please enter a date when you want to start your vacation in format DD-MM-YYYY: ')
-to_date = input('Please enter a date when you want to start your vacation in format DD-MM-YYYY: ')
+
+vacation_kind_input = None
+while True:
+    try:
+        vacation_kind_input = int(input('Please choose kind of vacation you want (enter a number)? '
+                                        '1 - Vacation, 2 - Sick leave, 3 - Day off: '))
+        break
+    except ValueError as e:
+        continue
+
+from_date_input = input('Please enter a date when you want to start your vacation: ')
+to_date_input = input('Please enter a date when you want to start your vacation: ')
 
 
-@vacation_types.get(vacation_kind_input)
-def vacation(name, surname, vacation_start, vacation_end):
-    print('\nCEO Red Bull Inc.\nMr. John Bigbull\n')
+@title
+def vacation(name, surname, vacation_start, vacation_end, vacation_kind):
+    """
+        This function creates a vacation request text based on the input parameters
+        and decorated by @title decorator
+    Args:
+        name: employee's name
+        surname: employee's surname
+        vacation_start: the first vacation date
+        vacation_end: the last vacation date
+        vacation_kind: type of requested vacation
+
+    Returns:
+        string - vacation body with args inserted
+    """
+    vacation_types = {
+        1: 'vacations',
+        2: 'sick leave',
+        3: 'day off',
+    }
+    if vacation_kind in vacation_types:
+        return f'\nHi John,\n' \
+               f'I need the paid {vacation_types.get(vacation_kind)} from {vacation_start} to {vacation_end}.\n' \
+               f'{name} {surname}'
+    return '\nI wanna get promotion :)'
 
 
-print(vacation(name_input, surname_input, from_date, to_date))
+print(vacation(name_input, surname_input, from_date_input, to_date_input, vacation_kind_input))
--- hw13/vacation_decorators.py
+++ hw13/vacation_decorators.py
@@ -1,25 +1,11 @@
-def vacation_type(func):
-    def wrapper(name, surname, vacation_start, vacation_end):
-        func(name, surname, vacation_start, vacation_end)
-        print(f'Hi John,\n'
-              f'I need the paid vacations from {vacation_start} to {vacation_end}.\n'
-              f'{name} {surname}')
-    return wrapper
-
-
-def sick_leave_type(func):
-    def wrapper(name, surname, vacation_start, vacation_end):
-        func(name, surname, vacation_start, vacation_end)
-        print(f'Hi John,\n'
-              f'I need the paid sick leave from {vacation_start} to {vacation_end}.\n'
-              f'{name} {surname}')
-    return wrapper
-
-
-def day_off_type(func):
-    def wrapper(name, surname, vacation_start, vacation_end):
-        func(name, surname, vacation_start, vacation_end)
-        print(f'Hi John,\n'
-              f'I need the paid day off from {vacation_start} to {vacation_end}.\n'
-              f'{name} {surname}')
+def title(func):
+    """
+        This decorator inserts the title of vacation request
+    Args:
+        func: function as object
+    Returns:
+        string - title concatenated with func execution
+    """
+    def wrapper(*args, **kwargs):
+        return '\nCEO Red Bull Inc.\nMr. John Bigbull\n' + func(*args, **kwargs)
     return wrapper
