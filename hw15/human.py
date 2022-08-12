7cef4a997b25820f291edd001bb1369aa802cbfb
Vadim Drozd <vadimdrozd@MacBook-Air-Vadim.local>
2022-08-12 16:09:27 +0300

    Added hw15

---
+++ hw15/human.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from human_sex import HumanSex
+
+
+@dataclass
+class Human:
+    name: str
+    surname: str
+    age: int
+
+    def __post_init__(self):
+        if not isinstance(self.name, str):
+            raise TypeError(f'Incorrect data type of "name" parameter: {type(self.name)}. Please use "str"')
+        if not isinstance(self.surname, str):
+            raise TypeError(f'Incorrect data type of "surname" parameter: {type(self.surname)}. Please use "str"')
+        if not isinstance(self.age, int):
+            raise TypeError(f'Incorrect data type of "age" parameter: {type(self.age)}. Please use "int"')
